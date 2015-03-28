7b873df641e5c91245f3fffd7c4f61684f9b343f
Heinz@FabFolk <heinz@fabfolk.com>
2015-03-28 02:51:21 +0100

    add descriptive pin names and remove global var

--- KnightRider.ino
+++ KnightRider.ino
@@ -9,25 +9,35 @@
  * @hardware: David Cuartielles, Aaron Hallborg
  */
 
-int pinArray[] = {2, 3, 4, 5, 6, 7};
-int count = 0;
+/* use more descriptive names for pin assignment */
+#define LED1    2
+#define LED2    3
+#define LED3    4
+#define LED4    5
+#define LED5    6
+#define LED6    7
+
+int pinArray[] = {PIN1, PIN2, PIN3, PIN4, PIN5, PIN6};
+#define MAX_LEDS    ( sizeof(pinArray) / sizeof(pinArray[0]) )
 int timer = 100;
 
 void setup(){
+  int count = 0;
   // we make all the declarations at once
-  for (count=0;count<6;count++) {
+  for (count=0;count<MAX_LEDS;count++) {
     pinMode(pinArray[count], OUTPUT);
   }
 }
 
 void loop() {
-  for (count=0;count<6;count++) {
+  int count = 0;
+  for (count=0;count<MAX_LEDS;count++) {
    digitalWrite(pinArray[count], HIGH);
    delay(timer);
    digitalWrite(pinArray[count], LOW);
    delay(timer);
   }
-  for (count=5;count>=0;count--) {
+  for (count=MAX_LEDS-1;count>=0;count--) {
    digitalWrite(pinArray[count], HIGH);
    delay(timer);
    digitalWrite(pinArray[count], LOW);
--- README.txt
+++ README.txt
@@ -8,5 +8,6 @@ Branch/Tag      ; Version
 ----------------;----------------------
 KnightRider1    ; Knight Rider 1
 KnightRider2    ; Knight Rider 2
+KnightRider2mod ; Knight Rider 2 - add descriptive pin names
 KnightRider3    ; Knight Rider 3
 shift           ; Shift LEDs - var number of bits; left/right; high/low active; shift-in/out
